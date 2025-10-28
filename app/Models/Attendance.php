14c6d7ecf98ac2de6272a4ca6081d1122081d3a3
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-10-28 22:13:30 +0700

    create attendance table migration and model

---
+++ app/Models/Attendance.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $table = 'attendance';
+
+    protected $fillable = [
+        'karyawan_id',
+        'tanggal',
+        'waktu_masuk',
+        'waktu_keluar',
+        'status_absensi',
+    ];
+}
