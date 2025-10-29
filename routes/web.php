78a7f1df3997d1acf25ea3bb8a922c49afadf18c
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-10-30 05:04:22 +0700

    add new views into route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\SalariesController;
+use App\Http\Controllers\PositionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 
 Route::resource('employees', EmployeeController::class);
+Route::resource('departments', DepartmentController::class);
+Route::resource('attendance', AttendanceController::class);
+Route::resource('salaries', SalariesController::class);
+Route::resource('positions', PositionController::class);
