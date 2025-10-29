7013bdf4be351e8112ff78804896ba5bd862a48d
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-10-30 04:57:05 +0700

    modify employee model adjusting to controller

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
+    public const STATUS_OPTIONS = [
+        'aktif',
+        'nonaktif',
+    ];
+
     protected $fillable = [
         'nama_lengkap',
         'email',
@@ -13,6 +21,28 @@ class Employee extends Model
         'tanggal_lahir',
         'alamat',
         'tanggal_masuk',
+        'departemen_id',
+        'jabatan_id',
         'status',
     ];
+    
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class, 'departemen_id');
+    }
+    
+    public function position(): BelongsTo
+    {
+        return $this->belongsTo(Position::class, 'jabatan_id');
+    }
+
+    public function salaries(): HasMany
+    {
+        return $this->hasMany(Salaries::class, 'karyawan_id');
+    }
+
+    public function latestSalary(): HasOne
+    {
+        return $this->hasOne(Salaries::class, 'karyawan_id')->latestOfMany();
+    }
 }
