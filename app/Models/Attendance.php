e55673d67688987814df4f3134dfcd839cf2bbe1
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-10-30 04:56:53 +0700

    modify attendance model adjusting to controller

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendance extends Model
 {
     protected $table = 'attendance';
+
+    public const STATUS_OPTIONS = [
+        'hadir',
+        'izin',
+        'sakit',
+        'alpha',
+    ];
 
     protected $fillable = [
         'karyawan_id',
@@ -15,4 +23,9 @@ class Attendance extends Model
         'waktu_keluar',
         'status_absensi',
     ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'karyawan_id');
+    }
 }
