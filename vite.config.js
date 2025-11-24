60a1606229b11dba7f1abfd1d7dff46e3f0e2505
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-11-24 15:13:59 +0700

    vite config for react, tailwindcss, and flowbite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
+    plugins: [laravel({
+        input: ['resources/ts/app.tsx'],
+        refresh: true,
+    }), react(), tailwindcss(), flowbiteReact()],
+    resolve: {
+        alias: {
+            '@': '/resources/ts'
         },
+    },
+    build: {
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom']
+                }
+            }
+        }
     }
 });
